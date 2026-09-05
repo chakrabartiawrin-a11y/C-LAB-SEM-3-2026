30c8f6464d5c09041c0ecd1d63cf2ed14ed28901
chakrabartiawrin-a11y <chakrabartiauryn@gmail.com>
2026-09-05 12:05:47 +0530

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{int n, r, sum=0, temp;
+cout<<"Enter a number:";
+cin>>n;
+temp=n;
+while(n>0)
+{ 
+    r=n%10;
+    sum=(sum*10)+r;
+    n=n/10;
+}
+if (temp==sum)
+cout<<"The number is a palindrome";
+else
+cout<<"The number is NOT a palindrome";
+return 0;
+}
